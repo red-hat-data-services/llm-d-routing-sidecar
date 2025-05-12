25e7f13986c3e98e3d4d103cafb5bf32eb4432c1
Lionel Villard <villard@us.ibm.com>
2025-05-12 18:20:02 -0400

    unit test and externalize connectors

---
+++ internal/proxy/connector_nixlv2.go
@@ -0,0 +1,165 @@
+/*
+Copyright 2025 IBM.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+func (s *Server) runNIXLProtocolV2(w http.ResponseWriter, r *http.Request, prefillPodURL string) {
+	s.logger.V(4).Info("running NIXL protocol V2", "url", prefillPodURL)
+
+	// Read request body
+	defer r.Body.Close() //nolint:all
+	original, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // TODO: check FastAPI error code when failing to read body
+		w.Write([]byte(err.Error()))         //nolint:all
+		return
+	}
+
+	// Parse completion request
+	var completionRequest map[string]any
+	if err := json.Unmarshal(original, &completionRequest); err != nil {
+		if err := errorJSONInvalid(err, w); err != nil {
+			s.logger.Error(err, "failed to send error response to client")
+		}
+		return
+	}
+
+	// Generate unique request UUID
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		if err := errorBadGateway(err, w); err != nil {
+			s.logger.Error(err, "failed to send error response to client")
+		}
+		return
+	}
+	uuidStr := uuid.String()
+
+	// Prefill Stage
+
+	// 1. Prepare prefill request
+	ctx := r.Context()
+	preq := r.Clone(ctx)
+
+	preq.Header.Add(RequestHeaderRequestID, uuidStr)
+
+	streamValue, streamOk := completionRequest[RequestFieldStream]
+	streamOptionsValue, streamOptionsOk := completionRequest[RequestFieldStreamOptions]
+	maxTokensValue, maxTokensOk := completionRequest[RequestFieldMaxTokens]
+
+	completionRequest[RequestFieldKVTransferParams] = map[string]any{
+		RequestFieldDoRemoteDecode:  true,
+		RequestFieldDoRemotePrefill: false,
+		RequestFieldRemoteEngineID:  nil,
+		RequestFieldRemoteBlockIDs:  nil,
+		RequestFieldRemoteHost:      nil,
+		RequestFieldRemotePort:      nil,
+	}
+
+	completionRequest[RequestFieldStream] = false
+	delete(completionRequest, RequestFieldStreamOptions)
+	completionRequest[RequestFieldMaxTokens] = 1
+
+	pbody, err := json.Marshal(completionRequest)
+	if err != nil {
+		if err := errorJSONInvalid(err, w); err != nil {
+			s.logger.Error(err, "failed to send error response to client")
+		}
+		return
+	}
+	preq.Body = io.NopCloser(strings.NewReader(string(pbody)))
+	preq.ContentLength = int64(len(pbody))
+
+	prefillHandler, err := s.prefillerProxyHandler(prefillPodURL)
+	if err != nil {
+		if err := errorBadGateway(err, w); err != nil {
+			s.logger.Error(err, "failed to send error response to client")
+		}
+		return
+	}
+
+	// 2. Forward request to prefiller
+	s.logger.V(5).Info("sending request to prefiller", "url", prefillPodURL, "body", string(pbody))
+	pw := &bufferedResponseWriter{}
+	prefillHandler.ServeHTTP(pw, preq)
+
+	if pw.statusCode < 200 || pw.statusCode >= 300 {
+		s.logger.Error(err, "request failed", "code", pw.statusCode)
+		w.WriteHeader(pw.statusCode)
+		return
+	}
+
+	// Process response - extract p/d fields
+	var prefillerResponse map[string]any
+	if err := json.Unmarshal([]byte(pw.buffer.String()), &prefillerResponse); err != nil {
+		if err := errorJSONInvalid(err, w); err != nil {
+			s.logger.Error(err, "failed to send error response to client")
+		}
+		return
+	}
+
+	// 3. Verify response
+
+	pKVTransferParams, ok := prefillerResponse[RequestFieldKVTransferParams]
+	if !ok {
+		s.logger.Info("warning: missing 'kv_transfer_params' field in prefiller response")
+	}
+
+	s.logger.V(5).Info("received prefiller response", RequestFieldKVTransferParams, pKVTransferParams)
+
+	// Decode Stage
+
+	// 1. Prepare decode request
+	dreq := r.Clone(ctx)
+
+	dreq.Header.Add(RequestHeaderRequestID, uuidStr)
+
+	delete(completionRequest, RequestFieldStream)
+	if streamOk {
+		completionRequest[RequestFieldStream] = streamValue
+	}
+	if streamOptionsOk {
+		completionRequest[RequestFieldStreamOptions] = streamOptionsValue
+	}
+	delete(completionRequest, RequestFieldMaxTokens)
+	if maxTokensOk {
+		completionRequest[RequestFieldMaxTokens] = maxTokensValue
+	}
+	completionRequest[RequestFieldKVTransferParams] = pKVTransferParams
+
+	dbody, err := json.Marshal(completionRequest)
+	if err != nil {
+		if err := errorJSONInvalid(err, w); err != nil {
+			s.logger.Error(err, "failed to send error response to client")
+		}
+		return
+	}
+	dreq.Body = io.NopCloser(strings.NewReader(string(dbody)))
+	dreq.ContentLength = int64(len(dbody))
+
+	// 2. Forward to local decoder.
+
+	s.logger.V(5).Info("sending request to decoder", "body", string(dbody))
+	s.decoderProxy.ServeHTTP(w, dreq)
+}
---
+++ internal/proxy/connector_nixlv2_test.go
@@ -0,0 +1,135 @@
+/*
+Copyright 2025 IBM.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package proxy
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/neuralmagic/llm-d-routing-sidecar/test/mock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/klog/v2/ktesting"
+)
+
+var _ = Describe("NIXL Connector (v2)", func() {
+
+	var (
+		ctx            context.Context
+		decodeBackend  *httptest.Server
+		decodeHandler  *mock.ChatCompletionHandler
+		prefillBackend *httptest.Server
+		prefillHandler *mock.ChatCompletionHandler
+		decodeURL      *url.URL
+		proxy          *Server
+	)
+
+	BeforeEach(func() {
+		_, ctx = ktesting.NewTestContext(GinkgoT())
+
+		// Decoder
+		decodeHandler = &mock.ChatCompletionHandler{
+			Connector: ConnectorNIXLV2,
+			Role:      mock.RoleDecode,
+		}
+		decodeBackend = httptest.NewServer(decodeHandler)
+		DeferCleanup(decodeBackend.Close)
+
+		// Prefiller
+		prefillHandler = &mock.ChatCompletionHandler{
+			Connector: ConnectorNIXLV2,
+			Role:      mock.RolePrefill,
+		}
+		prefillBackend = httptest.NewServer(prefillHandler)
+		DeferCleanup(prefillBackend.Close)
+
+		// Proxy
+		url, err := url.Parse(decodeBackend.URL)
+		Expect(err).ToNot(HaveOccurred())
+		decodeURL = url
+		proxy = NewProxy("0", decodeURL, ConnectorNIXLV2) // port 0 to automatically choose one that's available.
+	})
+
+	It("should successfully send request to 1. prefill 2. decode with the correct fields", func() {
+
+		By("starting the proxy")
+		go func() {
+			defer GinkgoRecover()
+
+			err := proxy.Start(ctx)
+			Expect(err).ToNot(HaveOccurred())
+		}()
+
+		time.Sleep(1 * time.Second)
+		Expect(proxy.addr).ToNot(BeNil())
+		proxyBaseAddr := "http://" + proxy.addr.String()
+
+		By("sending a /v1/chat/completions request with prefill header")
+		body := `{
+				"model": "Qwen/Qwen2-0.5B",
+				"messages": [
+				  {"role": "user", "content": "Hello"}
+				],
+				"max_tokens": 50
+			}`
+
+		req, err := http.NewRequest(http.MethodPost, proxyBaseAddr+ChatCompletionsPath, strings.NewReader(body))
+		Expect(err).ToNot(HaveOccurred())
+		req.Header.Add(RequestHeaderPrefillURL, prefillBackend.URL)
+
+		rp, err := http.DefaultClient.Do(req)
+		Expect(err).ToNot(HaveOccurred())
+
+		if rp.StatusCode != 200 {
+			bp, _ := io.ReadAll(rp.Body) //nolint:all
+			Fail(string(bp))
+		}
+
+		Expect(prefillHandler.RequestCount.Load()).To(BeNumerically("==", 1))
+
+		Expect(len(prefillHandler.CompletionRequests)).To(BeNumerically("==", 1))
+		prq1 := prefillHandler.CompletionRequests[0]
+
+		Expect(prq1).To(HaveKey(RequestFieldKVTransferParams))
+		kvTransferParams, ok := prq1[RequestFieldKVTransferParams].(map[string]any)
+		Expect(ok).To(BeTrue())
+
+		Expect(kvTransferParams).To(HaveKeyWithValue(RequestFieldDoRemoteDecode, true))
+		Expect(kvTransferParams).To(HaveKeyWithValue(RequestFieldDoRemotePrefill, false))
+		Expect(kvTransferParams).To(HaveKeyWithValue(RequestFieldRemoteBlockIDs, BeNil()))
+		Expect(kvTransferParams).To(HaveKeyWithValue(RequestFieldRemoteEngineID, BeNil()))
+		Expect(kvTransferParams).To(HaveKeyWithValue(RequestFieldRemoteHost, BeNil()))
+		Expect(kvTransferParams).To(HaveKeyWithValue(RequestFieldRemotePort, BeNil()))
+
+		Expect(prq1).To(HaveKeyWithValue("max_tokens", BeNumerically("==", 1)))
+		Expect(prq1).To(HaveKeyWithValue("stream", false))
+		Expect(prq1).ToNot(HaveKey("stream_options"))
+
+		Expect(len(prefillHandler.CompletionResponses)).To(BeNumerically("==", 1))
+		prp1 := prefillHandler.CompletionResponses[0]
+		Expect(prp1).To(HaveKey(RequestFieldKVTransferParams))
+
+		Expect(decodeHandler.RequestCount.Load()).To(BeNumerically("==", 1))
+		Expect(len(decodeHandler.CompletionRequests)).To(BeNumerically("==", 1))
+	})
+
+})
